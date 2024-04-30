fcf2c0315edeb24f19efbfde321003c6333b991b
Yash-032 <yashag0910@gmail.com>
2024-05-01 00:15:04 +0530

    Banner Done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,56 @@
+import { useState, useEffect } from "react"
 import { Col, Container, Row } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
 import headerImg from "../assets/img/header-img.svg"
 
 export const Banner = () => {
+    const toRotate = [ "Software Developer" , "Web Developer" , "UI/UX Designer"];
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    
+  const [index, setIndex] = useState(1);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+        return () => { clearInterval(ticker) };
+    }, [text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    
+        setText(updatedText);
+    
+        if (isDeleting) {
+          setDelta(prevDelta => prevDelta / 2);
+        }
+    
+        if (!isDeleting && updatedText === fullText) {
+          setIsDeleting(true);
+          setIndex(prevIndex => prevIndex - 1);
+          setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+          setIsDeleting(false);
+          setLoopNum(loopNum + 1);
+          setIndex(1);
+          setDelta(500);
+        } else {
+          setIndex(prevIndex => prevIndex + 1);
+        }
+      }
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my ProtFolio</span>
-                        <h1>{`Hi I'm Yash Agarwal`}<span className="wrap"></span></h1>
+                        <h1>{`Hi I'm Yash Agarwal `}<br></br><span className="wrap"> {text}</span></h1>
                         <p>
                         Passionate web developer proficient in crafting engaging web experiences with a strong foundation in Data Structures and Algorithms, adept at optimizing performance and solving complex problems efficiently.
                         </p>
