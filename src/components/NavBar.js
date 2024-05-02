bc6f29c21b649dee241d79731f3dcff2976fe6be
Yash-032 <yashag0910@gmail.com>
2024-05-02 13:48:14 +0530

    NavBar Changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,52 +1,56 @@
-import { Navbar , Container, Nav } from "react-bootstrap"
-import { useEffect, useState } from "react"
+import { Navbar , Container, Nav } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon3 from '../assets/img/icons8-x (2).svg';
 
+function openClick() {
+    window.open("https://github.com/Yash-032", "_blank");
+}
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
     
     useEffect(() => {
         const onScroll = () => {
-            if(window.scrollY > 50){
+            if(window.scrollY > 50) {
                 setScrolled(true);
-            } else{
+            } else {
                 setScrolled(false);
             }
-        }
-        window.addEventListener("scroll",onScroll);
-        return () => window.removeEventListener("scroll",onScroll);
-    }, [])
+        };
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
 
     const onUpdateActiveLink = (value) => {
         setActiveLink(value);
-    }
+    };
+
     return (
         <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-        <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a href="https://www.linkedin.com/in/yash-agarwal-a2b287270/" target="_blank"><img src={navIcon1} alt="" /></a>
-                <a href="https://twitter.com/Yagami_dn_" target="_blank"><img src={navIcon3} alt="" /></a>
-              </div>
-                    <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-                </span>
+            <Container>
+                <Navbar.Brand href="/">
+                    <img src={logo} alt="Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="https://www.linkedin.com/in/yash-agarwal-a2b287270/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="" /></a>
+                            <a href="https://twitter.com/Yagami_dn_" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="" /></a>
+                        </div>
+                        <button className="vvd" onClick={openClick}><span>Let's Connect</span></button>
+                    </span>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
-}
+    );
+};
