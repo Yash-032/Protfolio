d844b55e5f8075eaf16314f46781260305209e29
Yash-032 <yashag0910@gmail.com>
2024-05-05 18:52:49 +0530

    Resume Added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react"
-import { Col, Container, Row } from "react-bootstrap"
-import { ArrowRightCircle } from "react-bootstrap-icons"
-import headerImg from "../assets/img/header-img.svg"
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import YashResumePDF from '../assets/img/Resume.pdf';
 
 export const Banner = () => {
-    const toRotate = [ "Software Developer" , "Web Developer"];
+    const toRotate = ["Software Developer", "Web Developer"];
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
-    
-  const [index, setIndex] = useState(1);
+
+    const [index, setIndex] = useState(1);
     const period = 2000;
 
     useEffect(() => {
@@ -24,43 +25,46 @@ export const Banner = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
         let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-    
+
         setText(updatedText);
-    
+
         if (isDeleting) {
-          setDelta(prevDelta => prevDelta / 2);
+            setDelta(prevDelta => prevDelta / 2);
         }
-    
+
         if (!isDeleting && updatedText === fullText) {
-          setIsDeleting(true);
-          setIndex(prevIndex => prevIndex - 1);
-          setDelta(period);
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
         } else if (isDeleting && updatedText === '') {
-          setIsDeleting(false);
-          setLoopNum(loopNum + 1);
-          setIndex(1);
-          setDelta(500);
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
         } else {
-          setIndex(prevIndex => prevIndex + 1);
+            setIndex(prevIndex => prevIndex + 1);
         }
-      }
+    };
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my ProtFolio</span>
-                        <h1>{`Hi I'm Yash Agarwal `}<br></br><span className="wrap"> {text}</span></h1>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{`Hi I'm Yash Agarwal `}<br /><span className="wrap"> {text}</span></h1>
                         <p>
-                        Enthusiastic web developer with a solid background in Data Structures and Algorithms. Skilled at improving performance and finding effective solutions to challenging issues.
+                            Enthusiastic web developer with a solid background in Data Structures and Algorithms. Skilled at improving performance and finding effective solutions to challenging issues.
                         </p>
-                        <button onClick={()=> console.log('Connect')}>Download Resume<ArrowRightCircle size={25}/></button>
+                        {/* Button to trigger the download */}
+                        <a href={YashResumePDF} download="Yash_Resume" class="resume-link" >Download Resume <ArrowRightCircle size={25} /></a>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                            <img src={headerImg} alt="Headder Img"></img>
+                        <img src={headerImg} alt="Header Img" />
                     </Col>
                 </Row>
             </Container>
         </section>
-    )
-}
+    );
+};
+
+export default Banner;
